b09a39b0b5b3fa17045e62244b948114ef36ebf3
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-01-03 15:34:57 +0545

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
- const [inputname,setInputname] =useState("Raman ")
- 
-console.log(inputname)
- function handler(e){
+ const [inputname,setInputname] =useState("Raman")
+ const[increase,setIncrease]=useState(0)
+ const[decrease,setDecrease]=useState(0)
+console.log(increase,decrease)
+ // below code for useState
+// console.log(inputname)
+ function handler(e)
+{
   setInputname(e.target.value);
 
  }
-  return  (
+
+//below codes for useEffect
+useEffect(() => {
+  
+console.log("Raman")
+alert("yo  !!! ")
+}, [increase,decrease]);
+
+function incremental(){
+setIncrease(increase+1);
+}
+function decremental(){
+setDecrease(decrease-1)
+}
+
+  return  ( <div>
+
+   {/* Explaination of useSate hook */}
     
-   <div>
+    <div>
+ 
+ 
+     <h1>{inputname}</h1>
+ 
+     <input type ="text"  value= {inputname}  placeholder='Type your name here...' onChange={handler}/>
+    </div>
+ 
+    {/* EXPLAINNATION OF USEEFFECT HOOK */}
 
 
-    <h1>{inputname}</h1>
 
-    <input type ="text"  value= {inputname}  placeholder='Type your name here...' onChange={handler}/>
-   </div>
+    <button onClick={incremental}> Increment{increase} </button>
+    <button onClick={decremental}> Decrement {decrease}</button>
+
+
+
+
+
+
+  </div>
+
+   
+     
+
   )
 }
 
